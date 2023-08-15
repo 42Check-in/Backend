2132eae4700d05fe6a6f02e5f1dd8199f83fe449
saewoo1 <wooozooo1123@gmail.com>
2023-08-15 16:48:22 +0900

    [feat] : 초안

--- src/main/java/check_in42/backend/equipments/EquipmentController.java
+++ src/main/java/check_in42/backend/equipments/EquipmentController.java
@@ -1,30 +1,35 @@
 package check_in42.backend.equipments;
 
+import check_in42.backend.user.User;
+import check_in42.backend.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class EquipmentController {
     private final EquipmentService equipmentService;
+    private final UserService userService;
 
     @PostMapping("/equipments/form/new")
     public ResponseEntity createNewForm(@CookieValue String intraId, @RequestBody EquipmentDTO equipmentDTO) {
         Equipment equipment = equipmentService.create(intraId, equipmentDTO);
         Long equipmentFormId = equipmentService.join(equipment);
-        /*
-        * user에 이 form을 저장하는 부분이 필요함
-        * */
         return ResponseEntity.ok().body(equipmentFormId);
     }
 
 
+    /*
+     * 해당 user의 기존에 작성했던 모든 equipments form 보여주기?
+     * */
     @GetMapping("/equipments/form/extension")
-    public ResponseEntity<EquipmentDTO> showExtensionForm(@CookieValue String intraId) {
-
+    public List<EquipmentDTO> showExtensionForm(@CookieValue String intraId) {
+        return equipmentService.showAllFormByName(intraId);
     }
 
     /*
@@ -34,9 +39,13 @@ public class EquipmentController {
      * */
     @PostMapping("/equipments/form/extension")
     public ResponseEntity postExtensionForm(@CookieValue String intraId, @RequestBody EquipmentDTO equipmentDTO) {
-
+        equipmentService.extendDate(intraId, equipmentDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    /*
+     * 추가적으로 user의 equipList에서도 제거하기
+     * */
     @PostMapping("/equipments/cancel")
     public ResponseEntity cancel(@RequestParam Long formId) {
         equipmentService.findAndDelete(formId);
--- src/main/java/check_in42/backend/equipments/EquipmentService.java
+++ src/main/java/check_in42/backend/equipments/EquipmentService.java
@@ -1,40 +1,64 @@
 package check_in42.backend.equipments;
 
+import check_in42.backend.user.User;
+import check_in42.backend.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EquipmentService {
 
-    private final EquipmentRepository repository;
+    private final EquipmentRepository equipmentRepository;
+    private final UserRepository userRepository;
     public Long join(Equipment equipment) {
-        repository.save(equipment);
+        equipmentRepository.save(equipment);
 
         return equipment.getId();
     }
 
     public Equipment findOne(Long id){
-        return repository.findOne(id);
+        return equipmentRepository.findOne(id);
     }
 
     public void findAndDelete(Long id) {
-        Equipment equipment = repository.findOne(id);
-        repository.delete(equipment);
+        Equipment equipment = equipmentRepository.findOne(id);
+        equipmentRepository.delete(equipment);
     }
 
     public Equipment create(String intraId, EquipmentDTO equipmentDTO) {
-        return Equipment.builder()
-                .intraId(intraId)
-                .userName(equipmentDTO.getUserName())
-                .phoneNumber(equipmentDTO.getPhoneNumber())
-                .date(equipmentDTO.getDate())
-                .equipment(equipmentDTO.getEquipment())
-                .purpose(equipmentDTO.isPurpose())
-                .detail(equipmentDTO.getDetail())
-                .period(equipmentDTO.getPeriod())
-                .benefit(equipmentDTO.getBenefit())
-                .returnDate(equipmentDTO.getReturnDate())
-                .build();
+        return new Equipment(intraId, equipmentDTO);
+    }
+
+
+    /*
+     * intraid -> user 특정
+     * user가 갖고 있는 forms들을 모두 가져온 후 DTO에 맞춰서 생성, List에 담아서 내보내기
+     * */
+    public List<EquipmentDTO> showAllFormByName(String intraId) {
+        User user = userRepository.findByName(intraId);
+        List<Equipment> equipments = user.getEquipments();
+        List<EquipmentDTO> res = new ArrayList<>();
+
+        for (Equipment equip : equipments) {
+            res.add(EquipmentDTO.create(equip.getUserName(), equip.getPhoneNumber(), equip.getDate(), equip.getEquipment().ordinal(),
+                    equip.isPurpose(), equip.getDetail(), equip.getBenefit(), equip.getPeriod(), equip.getReturnDate()));
+        }
+        return res;
+    }
+
+    /*
+    *   1. String period
+        2. Sting returnDate
+        3. Long formId
+    * */
+    public void extendDate(String intraId, EquipmentDTO equipmentDTO) {
+        User user = userRepository.findByName(intraId);
+        /* user에 이 사항을 어케 업뎃할까 */
+        Equipment equipment = equipmentRepository.findOne(equipmentDTO.getFormId());
+        equipment.extendReturnDateByPeriod(equipmentDTO.getPeriod());
     }
 }
