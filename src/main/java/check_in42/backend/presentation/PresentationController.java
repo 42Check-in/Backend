5e6135f143d12e9b7e2ecd6ceb09cbb22db14fa6
saewoo1 <wooozooo1123@gmail.com>
2023-08-15 19:51:30 +0900

    [feat] : addService

--- src/main/java/check_in42/backend/presentation/PresentationController.java
+++ src/main/java/check_in42/backend/presentation/PresentationController.java
@@ -1,6 +1,7 @@
 package check_in42.backend.presentation;
 
 import check_in42.backend.presentation.utils.PresentationDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -8,12 +9,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class PresentationController {
+
+    private final PresentationService presentationService;
 
     //새로운 신청 폼 작성
     @PostMapping("/presentations/form")
     public ResponseEntity createNewForm(@CookieValue String intraId, @RequestBody PresentationDTO presentationDTO) {
+        Presentation presentation = presentationService.create(intraId, presentationDTO);
+        Long formId = presentationService.join(presentation);
 
+        return ResponseEntity.ok().body(formId);
     }
 
     //수요지식회 신청 현황 조회
--- src/main/java/check_in42/backend/presentation/PresentationService.java
+++ src/main/java/check_in42/backend/presentation/PresentationService.java
@@ -1,7 +1,33 @@
 package check_in42.backend.presentation;
 
+import check_in42.backend.presentation.utils.PresentationDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PresentationService {
+
+    private final PresentationRepository presentationRepository;
+
+    @Transactional
+    public Long join(Presentation presentation) {
+        presentationRepository.save(presentation);
+
+        return presentation.getId();
+    }
+
+    public void delete(Presentation presentation) {
+        presentationRepository.delete(presentation);
+    }
+
+    public Presentation findOne(Long id) {
+        return presentationRepository.findOne(id);
+    }
+
+    public Presentation create(String intraId, PresentationDTO presentationDTO) {
+
+    }
 }
