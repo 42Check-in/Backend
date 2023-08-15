867f2dcbda335874793119a4e72dd9dd4430be2c
saewoo1 <wooozooo1123@gmail.com>
2023-08-15 15:07:35 +0900

    [feat] : userService add

--- src/main/java/check_in42/backend/user/UserService.java
+++ src/main/java/check_in42/backend/user/UserService.java
@@ -1,7 +1,28 @@
 package check_in42.backend.user;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findOne(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    public Long join(User user) {
+        userRepository.save(user);
+        return user.getId();
+    }
+
+    public void delete(User user) {
+        userRepository.delete(user);
+    }
+
+    public User findByName(String intraId) {
+        return userRepository.findByName(intraId);
+    }
 }
