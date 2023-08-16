47f1c844fba7eec0e87ddd657d9e4b3a7809de1d
popomarin28@gmail.com <popomarin28@gmail.com>
2023-08-16 13:23:07 +0900

    [chore] : 주석 삽입

--- src/main/java/check_in42/backend/vocal/VocalController.java
+++ src/main/java/check_in42/backend/vocal/VocalController.java
@@ -1,5 +1,9 @@
 package check_in42.backend.vocal;
 
+import check_in42.backend.equipments.Equipment;
+import check_in42.backend.equipments.EquipmentService;
+import check_in42.backend.presentation.Presentation;
+import check_in42.backend.presentation.PresentationService;
 import check_in42.backend.visitors.Visitors;
 import check_in42.backend.visitors.VisitorsService;
 import check_in42.backend.vocal.utils.AppCategory;
@@ -14,22 +18,30 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/vocal/subscriptions")
 public class VocalController {
-    private final VisitorsService visitorsService;
 
+    private final VisitorsService visitorsService;
+    private final PresentationService presentationService;
+    private final EquipmentService equipmentService;
+
+    //모든 외부인 신청에 대한 조회 이지만, 갯수를 정할지 수락하지 않은 리스트만 보여줄지 정해야할듯
     @GetMapping("visitors")
     public ResponseEntity allVisitorsApply() {
             List<Visitors> visitorsList = visitorsService.findAll();
         return ResponseEntity.ok().body(visitorsList);
     }
 
+    // 전체 수요지식회 목록을 보여주는 기능 월별로 sort?
     @GetMapping("presentations")
     public ResponseEntity allPresentation() {
-
+        List<Presentation> presentationList = presentationService.findAll();
+        return ResponseEntity.ok(presentationList);
     }
 
+    // 전체 기자재 신청 목록을 보여주는 기능
     @GetMapping("equipments")
     public ResponseEntity allEquipment() {
-
+        List<Equipment> equipmentList = equipmentService.findAll();
+        return ResponseEntity.ok(equipmentList);
     }
 
     // 외부인 신청에 대한 수락
