7cbbe54f014b620355f12ea0515c695dbbf4d189
popomarin28@gmail.com <popomarin28@gmail.com>
2023-08-16 14:37:01 +0900

    [feat] : controller

---
+++ src/main/java/check_in42/backend/oauth/OauthController.java
@@ -0,0 +1,28 @@
+package check_in42.backend.oauth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class OauthController {
+
+    @GetMapping("login")
+    public ResponseEntity loginPage(HttpServletRequest request,
+                            @CookieValue(name = "intraId", required = false) String intraId) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            if (intraId == null) {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(HttpStatus.OK);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @PostMapping("login")
+    public ResponseEntity seoul42Login(@RequestParam("code") String code) {
+
+    }
+}
