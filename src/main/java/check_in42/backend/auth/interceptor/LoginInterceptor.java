05a0eb9e24194fc37fd95219afc11c5bd1fe88ea
popomarin28@gmail.com <popomarin28@gmail.com>
2023-08-21 17:31:07 +0900

    [log] : for logging

--- src/main/java/check_in42/backend/auth/interceptor/LoginInterceptor.java
+++ src/main/java/check_in42/backend/auth/interceptor/LoginInterceptor.java
@@ -3,11 +3,13 @@ package check_in42.backend.auth.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
 
     private final TokenInterceptor tokenInterceptor;
@@ -15,6 +17,7 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
+        log.info("들어오면 안되는데 왜 들어옴");
         if (TokenHeaderValidate.extractToken(request).isEmpty()) {
             return true;
         }
