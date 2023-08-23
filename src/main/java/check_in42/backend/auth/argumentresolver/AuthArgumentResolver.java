761851238c79f6dbc50da3f4112a90f8aa9c904f
popomarin28@gmail.com <popomarin28@gmail.com>
2023-08-23 20:33:13 +0900

    [test]

--- src/main/java/check_in42/backend/auth/argumentresolver/AuthArgumentResolver.java
+++ src/main/java/check_in42/backend/auth/argumentresolver/AuthArgumentResolver.java
@@ -31,7 +31,6 @@ public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         HttpServletRequest request = (HttpServletRequest)webRequest.getNativeRequest();
-        log.info("dfdfdfdfdfd");
         Optional<String> jwtToken = TokenHeaderValidate.extractToken(request);
         log.info(jwtToken.get());
         Claims claims = tokenProvider.parseAccessTokenClaim(jwtToken.get());
--- src/main/java/check_in42/backend/auth/interceptor/TokenHeaderValidate.java
+++ src/main/java/check_in42/backend/auth/interceptor/TokenHeaderValidate.java
@@ -1,16 +1,19 @@
 package check_in42.backend.auth.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Slf4j
 public class TokenHeaderValidate {
 
     public static Optional<String> extractToken(final HttpServletRequest request) {
         final String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info(authorization);
         if (Strings.isEmpty(authorization)) {
             return Optional.empty();
         }
