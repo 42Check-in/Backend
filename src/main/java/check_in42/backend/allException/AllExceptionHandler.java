170b1a0be02360cea7b9613d470082a122a4d1bb
popomarin28@gmail.com <popomarin28@gmail.com>
2023-08-24 17:00:42 +0900

    [test]

--- src/main/java/check_in42/backend/allException/AllExceptionHandler.java
+++ src/main/java/check_in42/backend/allException/AllExceptionHandler.java
@@ -2,6 +2,7 @@ package check_in42.backend.allException;
 
 import check_in42.backend.auth.exception.TokenException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,6 +14,6 @@ public class AllExceptionHandler {
     @ExceptionHandler(TokenException.class)
     private ResponseEntity handleTokenException(final CustomException e) {
         log.info(e.toString());
-        return ResponseEntity.status(e.getCode()).body(e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 }
--- src/main/java/check_in42/backend/auth/config/AuthConfig.java
+++ src/main/java/check_in42/backend/auth/config/AuthConfig.java
@@ -5,6 +5,7 @@ import check_in42.backend.auth.interceptor.LoginInterceptor;
 import check_in42.backend.auth.interceptor.TokenInterceptor;
 import check_in42.backend.auth.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerAdapter;
@@ -16,6 +17,7 @@ import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class AuthConfig implements WebMvcConfigurer {
     private final LoginInterceptor loginInterceptor;
     private final TokenInterceptor tokenInterceptor;
@@ -26,10 +28,13 @@ public class AuthConfig implements WebMvcConfigurer {
             "/presentation/*", "/visitors/*", "/equipments/*", "/vocal/*", "/oauth/login");
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
+        log.info("여기 됌?");
         registry.addInterceptor(loginInterceptor)
                 .addPathPatterns(LOGIN_URL);
+        log.info("여기임?");
         registry.addInterceptor(tokenInterceptor)
                 .addPathPatterns(TOKEN_URL);
+        log.info("씨바");
     }
 
     @Override
