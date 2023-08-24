03e0f70fbcf9849e2cd2614e92721544483498c8
popomarin28@gmail.com <popomarin28@gmail.com>
2023-08-24 22:00:08 +0900

    [refactor] : mycheckinController

--- src/main/java/check_in42/backend/myCheckIn/MyCheckInController.java
+++ src/main/java/check_in42/backend/myCheckIn/MyCheckInController.java
@@ -3,13 +3,17 @@ package check_in42.backend.myCheckIn;
 import check_in42.backend.auth.argumentresolver.UserId;
 import check_in42.backend.auth.argumentresolver.UserInfo;
 import check_in42.backend.conferenceRoom.ConferenceRoom.ConferenceRoom;
+import check_in42.backend.conferenceRoom.ConferenceRoom.ConferenceRoomService;
 import check_in42.backend.equipments.Equipment;
+import check_in42.backend.equipments.EquipmentService;
 import check_in42.backend.notice.utils.NoticeDTO;
 import check_in42.backend.presentation.Presentation;
+import check_in42.backend.presentation.PresentationService;
 import check_in42.backend.user.User;
 import check_in42.backend.user.UserService;
 import check_in42.backend.user.exception.UserRunTimeException;
 import check_in42.backend.visitors.Visitors;
+import check_in42.backend.visitors.VisitorsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +27,10 @@ public class MyCheckInController {
 
     private final UserService userService;
     private final MyCheckInService myCheckInService;
-
+    private final ConferenceRoomService conferenceRoomService;
+    private final VisitorsService visitorsService;
+    private final PresentationService presentationService;
+    private final EquipmentService equipmentService;
     @GetMapping("/conference-rooms")
     public ResponseEntity myConferenceRooms(@UserId final UserInfo userInfo) {
         final User user = userService.findByName(userInfo.getIntraId())
@@ -58,37 +65,27 @@ public class MyCheckInController {
     }
 
     @GetMapping("/conference-rooms/{formId}")
-    public ResponseEntity conferenceRoomForm(@PathVariable final Long formId,
-                                             @UserId final UserInfo userInfo) {
-        return ResponseEntity.ok(myCheckInService
-                .findConferenceFormFromUser(userService.findByName(userInfo.getIntraId())
-                        .orElseThrow(UserRunTimeException.NoUserException::new),
-                        formId));
+    public ResponseEntity conferenceRoomForm(@PathVariable final Long formId) {
+        final ConferenceRoom conferenceRoom = conferenceRoomService.findOne(formId);
+        return ResponseEntity.ok(conferenceRoom);
     }
 
     @GetMapping("/visitors/{formId}")
-    public ResponseEntity visitorForm(@UserId final UserInfo userInfo,
-                                      @RequestBody final NoticeDTO noticeDTO) {
-        return ResponseEntity.ok(myCheckInService
-                .findVisitorFormFromUser(userService.findByName(userInfo.getIntraId())
-                        .orElseThrow(UserRunTimeException.NoUserException::new), noticeDTO));
+    public ResponseEntity visitorForm(@PathVariable final Long formId) {
+        final Visitors visitors = visitorsService.findById(formId).get();
+        return ResponseEntity.ok(visitors);
     }
 
     @GetMapping("/presentations/{formId}")
-    public ResponseEntity presentationForm(@UserId final UserInfo userInfo,
-                                           @RequestBody final NoticeDTO noticeDTO) {
-        return ResponseEntity.ok(myCheckInService
-                .findPresentationFormFromUser(userService.findByName(userInfo.getIntraId())
-                        .orElseThrow(UserRunTimeException.NoUserException::new), noticeDTO));
-
+    public ResponseEntity presentationForm(@PathVariable final Long formId) {
+        final Presentation presentation = presentationService.findOne(formId);
+        return ResponseEntity.ok(presentation);
     }
 
     @GetMapping("/equipments/{formId}")
-    public ResponseEntity equipmentForm(@UserId final UserInfo userInfo,
-                                        @RequestBody final NoticeDTO noticeDTO) {
-        return ResponseEntity.ok(myCheckInService
-                .findEquipFormFromUser(userService.findByName(userInfo.getIntraId())
-                        .orElseThrow(UserRunTimeException.NoUserException::new), noticeDTO));
+    public ResponseEntity equipmentForm(@PathVariable final Long formId) {
+        final Equipment equipment = equipmentService.findOne(formId);
+        return ResponseEntity.ok(equipment);
     }
 
 }
