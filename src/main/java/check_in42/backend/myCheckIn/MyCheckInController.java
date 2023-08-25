adc7ff06a1d4f4f253f47d8d084307aa552e446d
saewoo1 <wooozooo1123@gmail.com>
2023-08-25 12:55:32 +0900

    [fix] MyCheckInController : entity -> DTO 반환

--- src/main/java/check_in42/backend/myCheckIn/MyCheckInController.java
+++ src/main/java/check_in42/backend/myCheckIn/MyCheckInController.java
@@ -3,17 +3,21 @@ package check_in42.backend.myCheckIn;
 import check_in42.backend.auth.argumentresolver.UserId;
 import check_in42.backend.auth.argumentresolver.UserInfo;
 import check_in42.backend.conferenceRoom.ConferenceRoom.ConferenceRoom;
+import check_in42.backend.conferenceRoom.ConferenceRoom.ConferenceRoomDTO;
 import check_in42.backend.conferenceRoom.ConferenceRoom.ConferenceRoomService;
 import check_in42.backend.equipments.Equipment;
+import check_in42.backend.equipments.EquipmentDTO;
 import check_in42.backend.equipments.EquipmentService;
 import check_in42.backend.notice.utils.NoticeDTO;
 import check_in42.backend.presentation.Presentation;
 import check_in42.backend.presentation.PresentationService;
+import check_in42.backend.presentation.utils.PresentationDTO;
 import check_in42.backend.user.User;
 import check_in42.backend.user.UserService;
 import check_in42.backend.user.exception.UserRunTimeException;
 import check_in42.backend.visitors.Visitors;
 import check_in42.backend.visitors.VisitorsService;
+import check_in42.backend.visitors.visitUtils.VisitorsDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -67,25 +71,25 @@ public class MyCheckInController {
     @GetMapping("/conference-rooms/{formId}")
     public ResponseEntity conferenceRoomForm(@PathVariable final Long formId) {
         final ConferenceRoom conferenceRoom = conferenceRoomService.findOne(formId);
-        return ResponseEntity.ok(conferenceRoom);
+        return ResponseEntity.ok(ConferenceRoomDTO.create(conferenceRoom));
     }
 
     @GetMapping("/visitors/{formId}")
     public ResponseEntity visitorForm(@PathVariable final Long formId) {
         final Visitors visitors = visitorsService.findById(formId).get();
-        return ResponseEntity.ok(visitors);
+        return ResponseEntity.ok(VisitorsDTO.create(visitors));
     }
 
     @GetMapping("/presentations/{formId}")
     public ResponseEntity presentationForm(@PathVariable final Long formId) {
         final Presentation presentation = presentationService.findOne(formId);
-        return ResponseEntity.ok(presentation);
+        return ResponseEntity.ok(PresentationDTO.create(presentation));
     }
 
     @GetMapping("/equipments/{formId}")
     public ResponseEntity equipmentForm(@PathVariable final Long formId) {
         final Equipment equipment = equipmentService.findOne(formId);
-        return ResponseEntity.ok(equipment);
+        return ResponseEntity.ok(EquipmentDTO.create(equipment));
     }
 
 }
