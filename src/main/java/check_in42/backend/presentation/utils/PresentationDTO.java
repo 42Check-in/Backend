42b308e22a038b145befd12e28fb8e3be7d9e042
popomarin28@gmail.com <popomarin28@gmail.com>
2023-08-29 00:10:49 +0900

    [fix] : status

--- src/main/java/check_in42/backend/presentation/utils/PresentationDTO.java
+++ src/main/java/check_in42/backend/presentation/utils/PresentationDTO.java
@@ -35,7 +35,8 @@ public class PresentationDTO {
         presentationDTO.time = PresentationTime.getOrdinalByDescription(presentation.getTime());
         log.info("_________________" + presentation.getStatus() + "____________________");
         log.info(presentation.getStatus() + "???????????????????????????");
-        presentationDTO.status = PresentationStatus.getOrdinalByDescription(presentation.getStatus());
+        presentationDTO.status = PresentationStatus.valueOf(presentation.getStatus()).ordinal();
+//        presentationDTO.status = PresentationStatus.getOrdinalByDescription(presentation.getStatus());
         log.info("이새키 몇이노?" + presentationDTO.status);
         presentationDTO.type = PresentationType.valueOf(presentation.getType()).ordinal();
         presentationDTO.screen = presentation.getScreen();
