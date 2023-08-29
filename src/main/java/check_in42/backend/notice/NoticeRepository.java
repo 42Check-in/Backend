a162f9b9ee3106cc076035b5e7fcba6b16959981
saewoo1 <wooozooo1123@gmail.com>
2023-08-29 20:31:57 +0900

    [fix] : visitor -> visitors

--- src/main/java/check_in42/backend/notice/NoticeRepository.java
+++ src/main/java/check_in42/backend/notice/NoticeRepository.java
@@ -17,7 +17,7 @@ public class NoticeRepository {
     public List<NoticeDTO> getNotice(Long userId) {
         Query query = entityManager.createNativeQuery(
                 "SELECT * FROM (" +
-                        "SELECT 'visitors' AS category, id AS form_id, approval, notice FROM visitor " +
+                        "SELECT 'visitors' AS category, id AS form_id, approval, notice FROM visitors " +
                         "WHERE user_id = :userId " +
                         "UNION " +
                         "SELECT 'equipment' AS category, id AS form_id, approval, notice FROM equipment " +
