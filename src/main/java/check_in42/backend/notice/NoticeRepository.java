2b0fd298119ee69cb8ab18f9ddc57858389059ee
saewoo1 <wooozooo1123@gmail.com>
2023-08-29 20:38:44 +0900

    [fix] test

--- src/main/java/check_in42/backend/notice/NoticeRepository.java
+++ src/main/java/check_in42/backend/notice/NoticeRepository.java
@@ -2,35 +2,30 @@ package check_in42.backend.notice;
 
 import check_in42.backend.notice.utils.NoticeDTO;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
+import jakarta.persistence.PersistenceContext;
+import org.hibernate.query.Query;
+import org.hibernate.transform.AliasToBeanResultTransformer;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
 public class NoticeRepository {
 
-    private final EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public List<NoticeDTO> getNotice(Long userId) {
-        Query query = entityManager.createNativeQuery(
-                "SELECT * FROM (" +
-                        "SELECT 'visitors' AS category, id AS form_id, approval, notice FROM visitors " +
-                        "WHERE user_id = :userId " +
-                        "UNION " +
-                        "SELECT 'equipment' AS category, id AS form_id, approval, notice FROM equipment " +
-                        "WHERE user_id = :userId " +
-                        "UNION " +
-                        "SELECT 'presentation' AS category, id AS form_id, approval, notice FROM presentation " +
-                        "WHERE user_id = :userId" +
-                        ") AS notice " +
-                        "WHERE approval IS NOT NULL " +
-                        "AND approval BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 3 DAY) " +
-                        "ORDER BY approval DESC", NoticeDTO.class
-        );
-        query.setParameter("userId", userId);
+        String nativeQuery = "SELECT 'visitors' AS category, id AS form_id, approval, notice FROM visitors " +
+                "WHERE user_id = :userId " +
+                "UNION " +
+                "SELECT 'equipment' AS category, id AS form_id, approval, notice FROM equipment " +
+                "WHERE user_id = :userId " +
+                "UNION " +
+                "SELECT 'presentation' AS category, id AS form_id, approval, notice FROM presentation " +
+                "WHERE user_id = :userId";
+
+        Query query = entityManager.createNativeQuery(nativeQuery)
+                .setParameter("userId", userId)
+                .unwrap(org.hibernate.query.NativeQuery.class)
+                .setResultTransformer(new AliasToBeanResultTransformer(NoticeDTO.class));
 
         List<NoticeDTO> results = query.getResultList();
         return results;
