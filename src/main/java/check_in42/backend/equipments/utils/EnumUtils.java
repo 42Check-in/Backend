928b9cc1c4a8aaee8a4c9a5c5e49bdd65cc36511
saewoo1 <wooozooo1123@gmail.com>
2023-08-30 16:06:32 +0900

    [test]

--- src/main/java/check_in42/backend/equipments/utils/EnumUtils.java
+++ src/main/java/check_in42/backend/equipments/utils/EnumUtils.java
@@ -8,7 +8,7 @@ public class EnumUtils {
     public static int getOrdinalByDescription(Class<? extends Enum<?>> enumValue, String description) {
         Map<String, Integer> descriptionToOrdinal = new HashMap<>();
 
-        for (Enum<?> enumConstant : getEnumConstants(enumValue)) {
+        for (Enum<?> enumConstant : enumValue.getEnumConstants()) {
             descriptionToOrdinal.put(getEnumDescription(enumConstant), enumConstant.ordinal());
         }
 
@@ -17,10 +17,6 @@ public class EnumUtils {
             return ordinalValue;
         }
         return 0; // Return a value indicating that the description was not found
-    }
-
-    private static Enum<?>[] getEnumConstants(Class<? extends Enum<?>> enumClass) {
-        return enumClass.getEnumConstants();
     }
 
     private static String getEnumDescription(Enum<?> enumConstant) {
