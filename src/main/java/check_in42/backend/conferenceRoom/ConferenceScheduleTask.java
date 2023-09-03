c40b82d265838febb21171cdfd52ea2e3b98c05f
이현수 <dldlsrn231@gmail.com>
2023-09-03 15:27:01 +0900

    [feat]: append conferenceScheduleTask

---
+++ src/main/java/check_in42/backend/conferenceRoom/ConferenceScheduleTask.java
@@ -0,0 +1,25 @@
+package check_in42.backend.conferenceRoom;
+
+import check_in42.backend.conferenceRoom.ConferenceRoom.ConferenceRoom;
+import check_in42.backend.conferenceRoom.ConferenceRoom.ConferenceRoomService;
+import check_in42.backend.user.User;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class ConferenceScheduleTask {
+
+    ConferenceRoomService conferenceRoomService;
+    User user;
+
+    @Scheduled(cron = "0 0 * * MON")
+    public void deleteConferenceRooms() {
+        conferenceRoomService.deleteAllByDateBeforeOneWeek();
+        List<ConferenceRoom> conferenceRooms = user.getConferenceRooms();
+        conferenceRooms.forEach(c -> {
+            if (c.getDate().isBefore(LocalDate.now().minusWeeks(1)))
+                user.getConferenceRooms().remove(c);
+        });
+    }
+}
