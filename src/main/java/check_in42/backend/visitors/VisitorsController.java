ae8df48e4bd37ca9e9a75ced21b021f84fca1af5
popomarin28@gmail.com <popomarin28@gmail.com>
2023-09-03 21:08:21 +0900

    [feat] : paging

--- src/main/java/check_in42/backend/visitors/VisitorsController.java
+++ src/main/java/check_in42/backend/visitors/VisitorsController.java
@@ -2,8 +2,10 @@ package check_in42.backend.visitors;
 
 import check_in42.backend.auth.argumentresolver.UserId;
 import check_in42.backend.auth.argumentresolver.UserInfo;
+import check_in42.backend.visitors.visitUtils.VisitorVocalResponse;
 import check_in42.backend.visitors.visitUtils.VisitorsDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,4 +29,17 @@ public class VisitorsController {
         visitorsService.delete(visitorsDTO.getFormId(), userInfo.getIntraId());
         return ResponseEntity.ok(HttpStatus.OK);
     }
+
+    @GetMapping("visitors/form/approval")
+    public ResponseEntity<VisitorVocalResponse> approvalForm(Pageable pageable) {
+        final VisitorVocalResponse visitorVocalResponse = visitorsService.findApprovalVisitorsList(pageable);
+        return ResponseEntity.ok(visitorVocalResponse);
+    }
+
+    @GetMapping("visitors/form/not-approval")
+    public ResponseEntity<VisitorVocalResponse> notApprovalForm(Pageable pageable) {
+        final VisitorVocalResponse visitorVocalResponse = visitorsService.findNotApprovalVisitorsList(pageable);
+        return ResponseEntity.ok(visitorVocalResponse);
+    }
+
 }
