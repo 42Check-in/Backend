39aeb5b71234a5e94eef3ac4c9cfcd126a1be5a7
이현수 <dldlsrn231@gmail.com>
2023-09-04 18:17:02 +0900

    [fix]: Table Enum

--- src/main/java/check_in42/backend/tablet/Rooms.java
+++ src/main/java/check_in42/backend/tablet/Rooms.java
@@ -7,14 +7,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Rooms {
-    CLUSTER1_1("cluster1_1", 0B01000001L << PlaceInfoBitSize.TIME.getValue()),
-    CLUSTER1_2("cluster1_2", 0B01000010L << PlaceInfoBitSize.TIME.getValue()),
-    CLUSTER_X("cluster_x", 0B01000100L << PlaceInfoBitSize.TIME.getValue()),
-    CLUSTER3_1("cluster3_1", 0B01001000L << PlaceInfoBitSize.TIME.getValue()),
-    CLUSTER3_2("cluster3_2", 0B01010000L << PlaceInfoBitSize.TIME.getValue()),
-    CLUSTER7("cluster7", 0B10000001L << PlaceInfoBitSize.TIME.getValue()),
-    CLUSTER9("cluster9", 0B10000010L << PlaceInfoBitSize.TIME.getValue());
+    CLUSTER1_1(0B01000001L << PlaceInfoBitSize.TIME.getValue()),
+    CLUSTER1_2(0B01000010L << PlaceInfoBitSize.TIME.getValue()),
+    CLUSTER_X(0B01000100L << PlaceInfoBitSize.TIME.getValue()),
+    CLUSTER3_1(0B01001000L << PlaceInfoBitSize.TIME.getValue()),
+    CLUSTER3_2(0B01010000L << PlaceInfoBitSize.TIME.getValue()),
+    CLUSTER7(0B10000001L << PlaceInfoBitSize.TIME.getValue()),
+    CLUSTER9(0B10000010L << PlaceInfoBitSize.TIME.getValue());
 
-    private final String roomName;
     private final Long roomBit;
 }
