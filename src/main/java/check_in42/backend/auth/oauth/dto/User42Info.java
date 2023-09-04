d08399335ad403150fb42a633365eaa521d6b700
saewoo1 <wooozooo1123@gmail.com>
2023-09-04 17:30:51 +0900

    [log]

--- src/main/java/check_in42/backend/auth/oauth/dto/User42Info.java
+++ src/main/java/check_in42/backend/auth/oauth/dto/User42Info.java
@@ -3,6 +3,7 @@ package check_in42.backend.auth.oauth.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -12,16 +13,18 @@ public class User42Info {
     private String login;
     @JsonProperty("staff?")
     private boolean staff;
+
     @JsonProperty("cursus_users")
     private List<CursusUser> cursus_users;
 
     @Getter
+    @Setter
     public static class CursusUser {
         @JsonProperty("cursus")
         private Cursus cursus;
 
 
-        @Getter
+        @Getter @Setter
         public static class Cursus {
             private String grade;
 
