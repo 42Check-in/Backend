01c6042475a268665fd1a3dbd6dad82f7d9355b9
saewoo1 <wooozooo1123@gmail.com>
2023-09-05 18:00:56 +0900

    [fix] : fix equipmentDTO

--- src/main/java/check_in42/backend/equipments/utils/EquipmentDTO.java
+++ src/main/java/check_in42/backend/equipments/utils/EquipmentDTO.java
@@ -31,20 +31,15 @@ public class EquipmentDTO {
         equipmentDTO.phoneNumber = equipment.getPhoneNumber();
         equipmentDTO.date = equipment.getDate().toString();
         equipmentDTO.formId = equipment.getId();
-        if ("42서울".equals(equipment.getPurpose())) {
-            equipmentDTO.purpose = 1;
-        }
-        else {
-            equipmentDTO.purpose = 0;
-            equipmentDTO.etcPurpose = equipment.getPurpose();
-        }
+        equipmentDTO.purpose = equipment.getPurpose().equals("42서울") ? 1 : 0;
+        equipmentDTO.etcPurpose = equipmentDTO.purpose == 0 ? equipment.getEquipment() : null;
         equipmentDTO.detail = equipment.getDetail();
         equipmentDTO.period = equipment.getPeriod();
         equipmentDTO.benefit = equipment.getBenefit();
         equipmentDTO.returnDate = equipment.getReturnDate().toString();
-        equipmentDTO.equipment =
-                EquipmentType.getOrdinalByDescription(equipment.getEquipment());
-        equipmentDTO.etcEquipment = equipment.getEquipment();
+        equipmentDTO.equipment = EquipmentType.getOrdinalByDescription(equipment.getEquipment());
+        equipmentDTO.etcEquipment = equipmentDTO.equipment == 0 ?
+                equipment.getEquipment() : null;
         equipmentDTO.status = equipment.getApproval() != null ? 1 : 0;
         equipmentDTO.extension = equipment.getExtension();
         equipmentDTO.intraId = equipment.getIntraId();
