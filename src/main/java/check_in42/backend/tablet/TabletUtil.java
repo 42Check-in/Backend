cff7c140511f7b568120c5137861d9ce7b30e235
이현수 <dldlsrn231@gmail.com>
2023-09-05 13:58:05 +0900

    [fix]

--- src/main/java/check_in42/backend/tablet/TabletUtil.java
+++ src/main/java/check_in42/backend/tablet/TabletUtil.java
@@ -8,6 +8,6 @@ public class TabletUtil {
 
         if (now.getHour() < 8)
             return 0;
-        return ((now.getHour() - 8) / 2) + (now.getMinute() >= 30 ? 1 : 0);
+        return ((now.getHour() - 8) * 2) + (now.getMinute() >= 30 ? 1 : 0);
     }
 }
